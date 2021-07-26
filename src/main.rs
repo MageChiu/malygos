c2f1fa3198ebc82a7d892625bee9e7027f1349c1
MageChiu <charles.r.chiu@gmail.com>
2021-07-26 16:37:07 +0800

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+/*
+ * @Author: Charles
+ * @LastEditors: Charles
+ * @Description: file content
+ */
+extern crate iron;
+#[macro_use]
+extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
     println!("Hello, world!");
+    println!("serving on thpp://localhost:8200");
+    Iron::new(get_from).http("localhost:8200").unwrap();
+}
+
+fn get_from(_request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(
+        r#"
+        <title> test </title>
+    
+    "#,
+    );
+
+    Ok(response)
 }
